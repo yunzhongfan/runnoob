f668c38eb739be0099619a23454b4e2567ca8c3f
yunzhongfan <763748729@qq.com>
2016-08-31 13:59:13 +0800

    创建项目runnoob项目

---
+++ servletTest/src/main/java/runnoob/servlet/servletTest.java
@@ -0,0 +1,55 @@
+package runnoob.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.Iterator;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+/**
+ * Servlet implementation class servletTest
+ */
+public class servletTest extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public servletTest() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		this.doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+       Enumeration  hearders =  request.getHeaderNames();
+       ArrayList<String> list = new ArrayList<String>();
+       for(;hearders.hasMoreElements();){
+           String thisName=hearders.nextElement().toString();
+           System.out.println(thisName);
+           list.add(thisName);
+       }
+       response.sendRedirect("/pages/success.jsp");
+       
+       
+	}
+
+}
